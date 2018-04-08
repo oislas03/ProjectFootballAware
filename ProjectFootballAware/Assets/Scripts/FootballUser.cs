41f8cad416707d0e5f53290ed4275464bad689b8
veronica1134 <veronica1134@hotmail.com>
2018-04-08 11:11:32 -0700

    Versión con Firebase y LocationService
    Versión donde ya se implementó la biblioteca de Firebase y sus funciones básicas así como también los LocationService de Unity.
    
    Falta poner la lógica de conexión, pasar datos e iniciar y terminar un juego apropiadamente.

---
+++ ProjectFootballAware/Assets/Scripts/FootballUser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootballUser
+{
+    public string username;
+    public string invited;
+    public string playing;
+    public string whoInvited;
+
+    public FootballUser()
+    {
+    }
+
+    public FootballUser(string username)
+    {
+        this.username = username;
+        this.invited = "no";
+        this.playing = "no";
+        this.whoInvited = "none";
+        
+    }
+}
---
+++ ProjectFootballAware/Assets/Scripts/LocationService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocationService : MonoBehaviour
+{
+    public Text qwer;
+    IEnumerator Start()
+    {
+        while (!UnityEditor.EditorApplication.isRemoteConnected)
+        {
+            Debug.Log("Unity Remote no está conectado");
+            yield break;
+        }
+
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("Servicio no activado");
+            yield break;
+        }
+
+        Input.location.Start(10.0f, 10.0f);
+
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait < 1)
+        {
+            Debug.Log("Se acabó el tiempo");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("No se puede determinar la ubicación");
+            yield break;
+        }
+        else
+        {
+            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        qwer.text = "Latitud: " + Input.location.lastData.latitude.ToString()
+                + "Longitud: " + Input.location.lastData.longitude.ToString()
+                + "Altitud: " + Input.location.lastData.altitude.ToString()
+                + "Exactitud horizontal: " + Input.location.lastData.horizontalAccuracy.ToString()
+                + "Tiempo: " + Input.location.lastData.timestamp.ToString();
+        Debug.Log("Latitud: " + Input.location.lastData.latitude
+                + "Longitud: " + Input.location.lastData.longitude
+                + "Altitud: " + Input.location.lastData.altitude
+                + "Exactitud horizontal: " + Input.location.lastData.horizontalAccuracy
+                + "Tiempo: " + Input.location.lastData.timestamp);
+    }
+}
